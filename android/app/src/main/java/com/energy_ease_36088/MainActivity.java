48bbe6c2e197adfeb50e1c492d71926d175aac84
Crowdbotics <team@crowdbotics.com>
2022-07-08 00:24:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/energy_ease_36088/MainActivity.java
@@ -0,0 +1,15 @@
+package com.energy_ease_36088;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "energy_ease_36088";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "energy_ease_36088",
+  url: "https://energy_ease_36088.botics.co",
+  api: "https://energy_ease_36088.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
